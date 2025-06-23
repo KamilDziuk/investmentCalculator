197711781a9de5e88485732cddf556c72afade21
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-06-23 23:58:25 +0200

    transferring elements of all header elements
    organization of Header styles and components in one folder (co-located styles)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
+import styles from "./Header.module.css";
 export default function Header() {
   return (
-    <header id="header">
+    <header id={styles.header}>
       <img src="investment_calculator_logo.webp" alt="" />
       <h1>Investment Calculator</h1>
     </header>
