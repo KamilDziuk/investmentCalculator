cb1c35b9a3ca40b89d815a97bb94d624db0e1942
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-06-23 23:59:54 +0200

    transferring elements of all results elements
    organization of Results styles and components in one folder (co-located styles)

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,18 +1,18 @@
-import { calculateInvestmentResults, formatter } from "../util/investment";
+import styles from "./Results.module.css";
+import {
+  calculateInvestmentResults,
+  formatter,
+} from "../../util/investment.js";
 
-export default function Result({ input }) {
+export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
-
   const initialInvestment =
     resultsData[0].valueEndOfYear -
     resultsData[0].interest -
     resultsData[0].annualInvestment;
-
-
-
   return (
-    <table id="result">
-  <thead>
+    <table id={styles.result}>
+      <thead>
         <tr>
           <th>Year</th>
           <th>Interest (Year)</th>
